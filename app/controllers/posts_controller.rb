803b71ab73942d61877e152deb475b88f39e1757
Brittany Laura Catone <bcatone@outlook.com>
2023-01-30 23:17:59 -0500

    commit tree d3ff6b24a04a7b3ebb171a458114ac6503a8fcc6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
   def show
     post = Post.find(params[:id])
     
@@ -8,10 +10,11 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
+    post.update!(post_params)
+    if post.valid?
+      render json: post, status: :ok
+    end
+  
   end
 
   private
@@ -19,5 +22,14 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+
+  def render_not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
+
 
 end
